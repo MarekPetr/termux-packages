5968cc609ce7350a323d3e5837dcfce7cbb26a8a
Petr Marek <petr.marek18@gmail.com>
2020-03-29 13:53:33 +0200

    Add built packages

--- extract.sh
+++ extract.sh
@@ -1,6 +1,7 @@
-for entry in $1"/debs"/*
+#Usage ./extract.sh [directory with debs folder] [destination folder]
+for entry in "debs"/*
 do
   echo "$entry"
-  dpkg-deb -x "$entry" $1/"git"
+  dpkg-deb -x "$entry" "./built/$1"
 done
 
